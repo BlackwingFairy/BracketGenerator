b470ed3b0be192fda62eaeeee25c27537f175d79
Ksenia Yakovchits <ksy.yakovchits@outlook.com>
2016-12-22 03:57:10 +0300

    normalised

--- generator/DataWorker.cs
+++ generator/DataWorker.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace generator
 {
